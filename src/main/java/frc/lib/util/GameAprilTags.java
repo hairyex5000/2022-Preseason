d1064a99241c5f17b7867b90d392f386ba724bdb
VincentShao32 <yunbo2016@gmail.com>
2023-01-10 23:50:12 -0800

    more accurate poses

--- src/main/java/frc/lib/util/GameAprilTags.java
+++ src/main/java/frc/lib/util/GameAprilTags.java
@@ -7,14 +7,14 @@ public class GameAprilTags {
     Pose2d[] targets = new Pose2d[8];
 
     public GameAprilTags () {
-        targets[0] = new Pose2d(15.38, 1.204, new Rotation2d());
-        targets[1] = new Pose2d(15.38, 2.601, new Rotation2d());
-        targets[2] = new Pose2d(15.38, 3.99, new Rotation2d());
+        targets[0] = new Pose2d(15.51, 1.07, new Rotation2d());
+        targets[1] = new Pose2d(15.51, 2.74, new Rotation2d());
+        targets[2] = new Pose2d(15.51, 4.42, new Rotation2d());
         targets[3] = new Pose2d();
         targets[4] = new Pose2d();
-        targets[5] = new Pose2d(1.03, 1.204, new Rotation2d(Math.PI));
-        targets[6] = new Pose2d(1.03, 2.601, new Rotation2d(Math.PI));
-        targets[7] = new Pose2d(1.03, 3.99, new Rotation2d(Math.PI));
+        targets[5] = new Pose2d(1.03, 4.42, new Rotation2d(Math.PI));
+        targets[6] = new Pose2d(1.03, 2.74, new Rotation2d(Math.PI));
+        targets[7] = new Pose2d(1.03, 1.07, new Rotation2d(Math.PI));
     }
 
     public Pose2d getPose(int id){
